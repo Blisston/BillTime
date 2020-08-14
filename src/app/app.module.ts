46278491e70137b6946b32552bf9aa56ee013352
Blisston kirubha <blisston.kirubha@inetconnexion.com>
2020-08-14 22:16:35 +0530

    calculations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BillComponent } from './bill/bill.component';
 import { PrintComponent } from './print/print.component';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +14,8 @@ import { PrintComponent } from './print/print.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/bill/bill.component.ts
+++ src/app/bill/bill.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-bill',
@@ -6,10 +6,105 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bill.component.css']
 })
 export class BillComponent implements OnInit {
-
-  constructor() { }
+  bills = `{
+    "bills":[{
+      "id":1,
+      "date":"12/09/2020",
+      "customer_data":{
+        "name":"Blisston Kirubha S",
+        "number":"8667084511"
+      },
+      "purchase":{
+        "items":[
+          {
+            "name":"Cashew Nut",
+            "unit_cost":"100",
+            "units":"8"
+          }],
+          "discount":"20",
+          "total":"10000"
+      }
+    }]
+  }`;
+  itemObj = `{
+    "items":[
+      {
+        "name":"almond",
+        "price":"80"
+      },
+      {
+        "name":"Pista",
+        "price":"90"
+      }
+    ]
+  }`;
+  items;
+  bill = {
+    id: 0,
+    date: new Date(),
+    customer_data: {
+      name: '',
+      number: ''
+    },
+    purchase: {
+      items: [],
+      discount: '',
+      total: ''
+    }
+  };
+  unitPrice = 0;
+  totalPrice = 0;
+  grandTotal = 0;
+  @ViewChild('inputForm') myForm;
+  constructor() {}
 
   ngOnInit(): void {
+    this.items = JSON.parse(this.itemObj).items;
+    this.bill.id = this.getId();
   }
 
+  getId(): number {
+    const bills = JSON.parse(this.bills).bills;
+    return bills[bills.length - 1].id + 1;
+  }
+  save(): void {
+    const bills = JSON.parse(this.bills).bills;
+    bills.push(this.bill);
+    console.log(bills);
+  }
+  getPrice(form) {
+    console.log(
+      this.items.filter(x => x.name === form.value.itemName)[0].price
+    );
+    this.unitPrice = this.items.filter(
+      x => x.name === form.value.itemName
+    )[0].price;
+    this.myForm.statusChanges.subscribe(result =>
+      this.calculateSub(this.myForm)
+    );
+  }
+  calculateSub(form) {
+    this.totalPrice = +this.unitPrice * +form.value.quantity;
+  }
+  getTotal(x, y) {
+    return x * y;
+  }
+  addItem(item) {
+    this.bill.purchase.items.push({
+      name: item.value.itemName,
+      unit_cost: '' + this.unitPrice,
+      units: '' + item.value.quantity
+    });
+    this.myForm.reset();
+    this.unitPrice = 0;
+    this.totalPrice = 0;
+    this.calculateGrand();
+  }
+
+  calculateGrand() {
+    this.grandTotal = 0;
+    this.bill.purchase.items.forEach(x => {
+      this.grandTotal += this.getTotal(+x.units, +x.unit_cost);
+    });
+  }
 }
